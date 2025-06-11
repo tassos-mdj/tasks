523850f139523542752ec7ff4e6f25d79e8d9282
tassos-mdj <164147506+tassos-mdj@users.noreply.github.com>
2025-06-11 18:17:24 +0300

    Prepare webpack config for production

--- webpack.common.js
+++ webpack.common.js
@@ -2,16 +2,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-    mode: "development",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"],
     },
     plugins: [
         new HtmlWebpackPlugin({
